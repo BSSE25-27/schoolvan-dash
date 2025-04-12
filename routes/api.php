a5b6fd31b4c6e64defd96d843fba35aec6c1e75d
joshuamumbere <joshuamumbere71@gmail>
2025-04-12 23:57:27 +0300

    Implemented vanoperator login api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ParentalController;
 use App\Models\Parental;
+use App\Models\Operator;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -11,3 +13,4 @@ Route::get('/user', function (Request $request) {
 Route::get('/parents/{parents}', [ParentalController::class, 'show']);
 Route::get('/parents', [ParentalController::class, 'index']);
 Route::post('/parent-login', [ParentalController::class, 'parentLogin']);
+Route::get('/operator-login', [OperatorController::class, 'operatorLogin']);
