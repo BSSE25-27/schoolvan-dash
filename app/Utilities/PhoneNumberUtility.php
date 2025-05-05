d53c313ea16d8e69c9bf2e29e5161f7a8cd01376
joshuamumbere <joshuamumbere71@gmail>
2025-05-05 22:45:03 +0300

    Added the utility for creating right phone number format

---
+++ app/Utilities/PhoneNumberUtility.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Utilities;
+
+class PhoneNumberUtility
+{
+    public static function formatForSms($phoneNumber)
+    {
+        // Remove all non-digit characters
+        $cleaned = preg_replace('/[^0-9]/', '', $phoneNumber);
+        
+        // If starts with 0, replace with +256
+        if (strlen($cleaned) > 9 && $cleaned[0] === '0') {
+            return '+256' . substr($cleaned, 1);
+        }
+        
+        // If starts with 256 without +, add +
+        if (strlen($cleaned) > 9 && substr($cleaned, 0, 3) === '256') {
+            return '+' . $cleaned;
+        }
+        
+        // If already in international format, return as is
+        if (strlen($cleaned) > 9 && $cleaned[0] === '+') {
+            return $cleaned;
+        }
+        
+        // Default return (shouldn't happen for valid UG numbers)
+        return $phoneNumber;
+    }
+}
